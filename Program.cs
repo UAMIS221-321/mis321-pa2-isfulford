40802ccf03ef9b646ffe0dc9517290256cddc859
Ivy Fulford <ivyfulford1@gmail.com>
2022-03-01 12:10:41 -0600

    2nd

--- Program.cs
+++ Program.cs
@@ -17,35 +17,57 @@ namespace mis321_pa2_isfulford
             characterCount++;
             p2.UserName = PlayerInfo(characterCount);
             p2 = CharacterChoice(p2);
-            
-            // Console.WriteLine(p1.AttackStrength);
-            // Console.WriteLine(p1.DefensivePower);
-
-            // Console.WriteLine(p2.AttackStrength);
-            // Console.WriteLine(p2.DefensivePower);
 
             Console.WriteLine("Let us see who will go first");
             //uses randomNum to choose who goes first
             //p1 goes 1st
             if(p1.AttackStrength < p2.AttackStrength){
                 while(p1.Health > 0 && p2.Health > 0){
-                    double defaultHealthDeduction = .10;
+                    double defaultHealthDeduction = .15;
 
                     Console.WriteLine("Player 1 Fight!");
+                    //if the defender's defPower is higher than attacker's attackStrength, take 15% of their health
                     if(p2.DefensivePower > p1.AttackStrength){
-                        p2.Health -= (p2.Health * defaultHealthDeduction);
+                        if((p1.Name == "Jack Sparrow" && p2.Name == "Will Turner")||(p1.Name == "Will Turner" && p2.Name == "Davy Jones")
+                            ||(p1.Name == "Davy Jones" && p2.Name == "Jack Sparrow")){
+                            p2.Health -= (p2.Health * defaultHealthDeduction) * 1.2;
+                        }
+                        else{
+                            p2.Health -= (p2.Health * defaultHealthDeduction);
+                        }
                     }
                     else{
-                        p2.Health -= (p1.AttackStrength - p2.DefensivePower);
+                        //Type Bonus
+                        if((p1.Name == "Jack Sparrow" && p2.Name == "Will Turner")||(p1.Name == "Will Turner" && p2.Name == "Davy Jones")
+                        ||(p1.Name == "Davy Jones" && p2.Name == "Jack Sparrow")){
+                            p2.Health -= (p1.AttackStrength - p2.DefensivePower)* 1.2;
+                            Console.WriteLine("I used my type bonus");
+                        }
+                        else{
+                            p2.Health -= (p1.AttackStrength - p2.DefensivePower);
+                        }
                     }
+                    Console.WriteLine($"{p1.Name} {p1.attackBehavior}");
                     ViewStats(p1.Health,p2.Health);
                     
                     Console.WriteLine("Player 2 Fight!");
                     if(p1.DefensivePower > p2.AttackStrength){
-                        p1.Health -= (p1.Health * defaultHealthDeduction);
+                        if((p2.Name == "Jack Sparrow" && p1.Name == "Will Turner")||(p2.Name == "Will Turner" && p1.Name == "Davy Jones")
+                            ||(p2.Name == "Davy Jones" && p1.Name == "Jack Sparrow")){
+                            p1.Health -= (p1.Health * defaultHealthDeduction) * 1.2;
+                        }
+                        else{
+                            p1.Health -= (p1.Health * defaultHealthDeduction);
+                        }
                     }
                     else{
-                        p1.Health -= (p2.AttackStrength - p1.DefensivePower);
+                        if((p2.Name == "Jack Sparrow" && p1.Name == "Will Turner")||(p2.Name == "Will Turner" && p1.Name == "Davy Jones")
+                        ||(p2.Name == "Davy Jones" && p1.Name == "Jack Sparrow")){
+                            p1.Health -= (p2.AttackStrength - p1.DefensivePower)* 1.2;
+                        }
+                        else{
+                            p1.Health -= (p2.AttackStrength - p1.DefensivePower);
+                        }
                     }
                     ViewStats(p1.Health,p2.Health);
                 }
@@ -54,23 +76,51 @@ namespace mis321_pa2_isfulford
             //p2 goes 1st
             if(p2.AttackStrength < p1.AttackStrength){
                 while(p2.Health > 0 && p1.Health > 0){
-                    double defaultHealthDeduction = .10;
+                    double defaultHealthDeduction = .15;
  
                     Console.WriteLine("Player 2 Fight!");
+                    //if the defender's defPower is higher than attacker's attackStrength, take 15% of their health
                      if(p1.DefensivePower > p2.AttackStrength){
-                        p1.Health -= (p1.Health * defaultHealthDeduction);
+                        if((p2.Name == "Jack Sparrow" && p1.Name == "Will Turner")||(p2.Name == "Will Turner" && p1.Name == "Davy Jones")
+                            ||(p2.Name == "Davy Jones" && p1.Name == "Jack Sparrow")){
+                            p1.Health -= (p1.Health * defaultHealthDeduction) * 1.2;
+                        }
+                        else{
+                            p1.Health -= (p1.Health * defaultHealthDeduction);
+                        }
                     }
                     else{
-                        p1.Health -= (p2.AttackStrength - p1.DefensivePower);
+                        //Type Bonus
+                        if((p2.Name == "Jack Sparrow" && p1.Name == "Will Turner")||(p2.Name == "Will Turner" && p1.Name == "Davy Jones")
+                        ||(p2.Name == "Davy Jones" && p1.Name == "Jack Sparrow")){
+                            p1.Health -= (p2.AttackStrength - p1.DefensivePower)* 1.2;
+                        }
+                        else{
+                            p1.Health -= (p2.AttackStrength - p1.DefensivePower);
+                        }
                     }
                     ViewStats(p1.Health,p2.Health);
 
                     Console.WriteLine("Player 1 Fight!");
                     if(p2.DefensivePower > p1.AttackStrength){
+                        if((p1.Name == "Jack Sparrow" && p2.Name == "Will Turner")||(p1.Name == "Will Turner" && p2.Name == "Davy Jones")
+                            ||(p1.Name == "Davy Jones" && p2.Name == "Jack Sparrow")){
+                            // EXTRA (per Jeff reccommendation), add 20% on top of default 10% deduction
+                            p2.Health -= (p2.Health * defaultHealthDeduction) * 1.2;
+                        }
+                        else{
                         p2.Health -= (p2.Health * defaultHealthDeduction);
+                        }
                     }
                     else{
-                        p2.Health -= (p1.AttackStrength - p2.DefensivePower);
+                        //Type Bonus
+                        if((p1.Name == "Jack Sparrow" && p2.Name == "Will Turner")||(p1.Name == "Will Turner" && p2.Name == "Davy Jones")
+                            ||(p1.Name == "Davy Jones" && p2.Name == "Jack Sparrow")){
+                            p2.Health -= (p1.AttackStrength - p2.DefensivePower)* 1.2;
+                        }
+                        else{
+                            p2.Health -= (p1.AttackStrength - p2.DefensivePower);
+                        }
                     }
                     ViewStats(p1.Health,p2.Health);
                 }
@@ -89,11 +139,13 @@ namespace mis321_pa2_isfulford
             string userName = Console.ReadLine();
 
             return userName;
+
         }
         public static Character CharacterChoice(Character playerCharacter){
             Console.WriteLine("Please choose a player from the following menu");
             Console.WriteLine("1) Jack Sparrow 2) Will Turner 3) Davy Jones");
             int choice = int.Parse(Console.ReadLine());
+            string tempUserName = playerCharacter.UserName;
 
             if(choice == 1){
                 playerCharacter = new JackSparrow();
@@ -102,11 +154,15 @@ namespace mis321_pa2_isfulford
             else if(choice == 2){
                 playerCharacter = new WillTurner();
                 playerCharacter.Name = "Will Turner";
+
             }
             else{
                 playerCharacter = new DavyJones();
                 playerCharacter.Name = "Davy Jones";
             }
+            
+            playerCharacter.UserName = tempUserName;
+
             
             return playerCharacter;
         }
